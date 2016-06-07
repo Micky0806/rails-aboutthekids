8bb3e37caf10d8ffbb5cf900da1f100e6882f49e
Mike Hanot <mikehanot@gmail.com>
2016-06-07 12:18:16 +0200

    add columns to user and migrate

---
+++ db/migrate/20160607101144_add_colums_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :owner, :boolean
+    add_column :users, :photo, :string
+    add_column :users, :company_name, :string
+    add_column :users, :phone_number, :string
+  end
+end
