46fd03fc617e8516d5074ffb1e91e1db7378f7cf
Mike Hanot <mikehanot@gmail.com>
2016-06-08 16:28:52 +0200

    add method index still a problem, show, new and create

--- app/controllers/owner/activities_controller.rb
+++ app/controllers/owner/activities_controller.rb
@@ -1,7 +1,43 @@
 class Owner::ActivitiesController < ApplicationController
-
+  before_action :find_activity, only: [ :show, :edit, :update, :destroy ]
   def index
-    @activities = Activity.all
+    @activities = current_user.activities
   end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+    @owner = User.find(current_user.id)
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.save!
+    redirect_to owner_activity_path(@activity)
+  end
+
+  def edit
+  end
+
+  def update
+
+
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def find_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+     params.require(:activity).permit(:name, :photo, :photo_cache, :description, :category, :address, :city, :period_beginning, :period_ending, :price )
+   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
++Activity.destroy_all
+ +
+ +Activity.create(
+ +  name: "Basketball",
+ +  description: "Always wanted to feel like Michael Jordan?",
+ +  photo: "http://www.windailyfantasy.com/wp-content/uploads/2015/02/Playing-College-Hoops.png",
+ +  city: "Amsterdam",
+ +  address: "Admiraal de ruijterweg 296",
+ +  period_beginning: Date.new(2016,2,3),
+ +  period_ending: Date.new(2017,1,5)
+ +  )
+ +Activity.create(
+ +  name: "Painting",
+ +  description: "The best fingerpaiting lessons in Amsterdam",
+ +  photo: "http://www.windailyfantasy.com/wp-content/uploads/2015/02/Playing-College-Hoops.png",
+ +  city: "Amsterdam",
+ +  address: "Europaplein 57",
+ +  period_beginning: Date.new(2016,2,3),
+ +  period_ending: Date.new(2017,1,5)
+ +  )
+ +Activity.create(
+ +  name: "Piano lessons",
+ +  description: "Learing to play the piano is never been more fun",
+ +  photo: "http://www.windailyfantasy.com/wp-content/uploads/2015/02/Playing-College-Hoops.png",
+ +  city: "Amsterdam",
+ +  address: "James Wattstaat 75",
+ +  period_beginning: Date.new(2016,2,3),
+ +  period_ending: Date.new(2017,1,5)
+ +  )
