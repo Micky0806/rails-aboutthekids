9d9f34407ef25e2641c77aef21cadacc60b2a0a6
Jurjen Munneke <jurjenmunneke@me.com>
2016-06-08 14:07:54 +0200

    Fix unexpected end error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,6 @@ class User < ActiveRecord::Base
          has_many :activities
          has_many :orders, through: :activities, dependent: :destroy, class_name: 'booking'
 
-         end
-
-
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
