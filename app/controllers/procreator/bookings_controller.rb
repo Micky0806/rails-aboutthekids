1c1259c7c314ec3ccdbb6d29cfaba4c80df965d3
jesse ramon <jramonibz555@gmail.com>
2016-06-08 16:51:45 +0200

    add procreator bookings

--- app/controllers/procreator/bookings_controller.rb
+++ app/controllers/procreator/bookings_controller.rb
@@ -1,2 +1,11 @@
 class Procreator::BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(current_user.id)
+  end
+
 end
