96dd086f1127bf0393853ccf6054838c42f826f1
jesse ramon <jramonibz555@gmail.com>
2016-06-08 12:13:52 +0200

    add children view

--- app/controllers/procreator/children_controller.rb
+++ app/controllers/procreator/children_controller.rb
@@ -1,2 +1,19 @@
 class Procreator::ChildrenController < ApplicationController
+
+  def index
+    @children = Child.all
+  end
+
+  def new
+    @child = Child.new
+  end
+
+  def create
+    @child = Child.new(params.require(:child).permit(:name, :age))
+     @child.save!
+     redirect_to child_path(@child)
+  end
+
+
 end
+
