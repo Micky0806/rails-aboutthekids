2afa2dde241dfeabf33f0346858360937d711567
Jurjen Munneke <jurjenmunneke@me.com>
2016-06-09 12:12:18 +0200

    Make the profile edit work

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,10 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user = current_user
+    @user.update(params.require(:user).permit(:first_name, :last_name, :phone_number, :company_name))
+    redirect_to profile_path(@user)
+    # flash.now[:alert] = "hallo probeer opnieuw" ==> zie shared/flashes
   end
 
 end
