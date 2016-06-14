77b205ca8ede87d1e51759a29bbab55cca019a42
Mike Hanot <mikehanot@gmail.com>
2016-06-14 20:58:56 +0200

    add card to the activities index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,15 +2,15 @@ class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @activities = Activity.all
+   @activities = Activity.all
     @markers = Gmaps4rails.build_markers(@activities) do |activity, marker|
      marker.lat activity.latitude
      marker.lng activity.longitude
    end
  end
 
-   def show
-    @activity = Activity.find(params[:id])
-    @activity_coordinates = { lat: @activity.latitude, lng: @activity.longitude }
-  end
+ def show
+  @activity = Activity.find(params[:id])
+  @activity_coordinates = { lat: @activity.latitude, lng: @activity.longitude }
+end
 end
